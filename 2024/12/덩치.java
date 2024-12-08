1435de9e86f4d48dedaaba4411684d9795c03f6d
tmddus2123 <52913611+tmddus2123@users.noreply.github.com>
2024-12-08 18:06:36 +0900

    Create 덩치.java

---
+++ 2024/12/덩치.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int num = Integer.parseInt(br.readLine());
+        int[][] arr = new int[num][2];
+
+        for(int i=0; i<num; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=0; i<num; i++) {
+            int rank = 1;
+            for(int j=0; j<num; j++) {
+                if(i == j) {
+                    continue;
+                }
+                if(arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
+                    rank++;
+                }
+            }
+            sb.append(rank).append(" ");
+        }
+        System.out.print(sb);
+    }
+}
